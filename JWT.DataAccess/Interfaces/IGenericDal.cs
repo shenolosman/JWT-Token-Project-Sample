9cdba339b16291128519f39bf9f60038f2fc4f56
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-05-20 15:56:02 +0200

    Creating Generic Interface for entityframework

---
+++ JWT.DataAccess/Interfaces/IGenericDal.cs
@@ -0,0 +1,16 @@
+﻿using JWT.Entities.Interfaces;
+using System.Linq.Expressions;
+
+namespace JWT.DataAccess.Interfaces
+{
+    public interface IGenericDal<TEntity> where TEntity : class, ITable, new()
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<List<TEntity>> GetAllAsyncByFilter(Expression<Func<TEntity, bool>> filter);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<TEntity> GetAsyncByFilter(Expression<Func<TEntity, bool>> filter);
+        Task DeleteAync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task AddAsync(TEntity entity);
+    }
+}
