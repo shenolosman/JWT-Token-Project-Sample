1300242ee9b5c3d60142b853a646adafa4acd5c1
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-05-21 11:35:52 +0200

    Linkid dependencies and created product controller

---
+++ JWT.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using JWT.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this._productService = productService;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            _productService.GetAll();
+            return Ok();
+        }
+    }
+}
