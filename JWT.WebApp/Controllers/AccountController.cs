b5a6b1add97987d0a77a3f707620374abbcc8550
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-06-16 11:46:03 +0200

    creating accountcontroller and login page

---
+++ JWT.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWT.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JWT.WebApp/Models/AppUserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT.WebApp.Models
+{
+    public class AppUserLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
